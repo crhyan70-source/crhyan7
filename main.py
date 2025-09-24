61dd91463de06485a51ebb860f82678ad0a41dc1
rhyan <crhyan70@gmail.com>
2025-09-24 16:38:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+# Multiplique 4 por 6 e divida o resultado por 2. 
+# (Concatene com a palavra resultado)
+
+calculo  =  (4 * 6)/2
+print('Resultado', calculo)
+
+
+# Calcule a soma de 5 e 7.
+# (Concatene com a palavra resultado)
+
+soma  =  5 + 7
+print('resultado', soma)
+
+# Eleve 3 à potência de 4.
+# (Concatene com a palavra resultado)
+
+potencia  =  3 ** 4
+print('Resultado', potencia)
+
+# Calcule a média de 8, 12 e 15.
+# (Concatene com a palavra resultado)
+
+media  = (8 + 12 + 15)/3
+print('Resultado', media)
+
+
+# Subtraia 10 de 2 e multiplique o resultado por 
+# 5.(Concatene com a palavra resultado)
+
+subtracao =  (2 - 10) * 5
+print('Resultado', subtracao)
+
+
+# Divida 27 por 3 e some 5.
+# (Concatene com a palavra resultado)
+
+# p(arentes)e(exponenciação)m(multiplicação)d(divisão)a(adição)s(soma) 
+div  =  27 / 3 + 5 
+
+
+# Exercícios com variáveis / print() / input()(opcional):
+
+# Calcule o módulo ( % ) de 17 por 4.
+
+mod  = 17 % 4
+print('Resuldo módulo', mod)
+
+
+# Multiplique 9 por 3 e, em seguida, 
+# eleve o resultado ao quadrado.
+m = ( 9 * 3 ) ** 2
+
+
+# Calcule a raiz quadrada de 81
+
+raiz = 81 ** 0.5
+print('Resultado Raiz', raiz) 
+
+# Adicione 20 a 3 vezes 4.
+
+calculo = (20 + 3) * 4
+print('Resultado',calculo)
+
+
+# Multiplique 15 por 2 e, em seguida, subtraia 7.
+multiplicacao =  15 * 2 - 7
+
+# Eleve 5 ao cubo.
+
+cubo = 5 ** 3
+print('Resultado cubo', cubo)
+# Calcule a média de 17, 21 e 25.
+
+media2 = 17 + 21 + 25
+print('Resultado Multiplicação', media2/3)
+
+# Multiplique 11 por 2 e adicione 7.
+m = 11 * 2 + 7
+print('Resultado Multiplicação', m)
+
+# Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+
+subt = (( 3 * 8 )-15)/2
+print('Resultado subtração', subt)
+
+# Eleve 2 à potência de 10.
+
+potencia = 2 ** 10
+print('Resultado',potencia)
